ac8c3210cf9e2e2aea575bc5cc6fe60a723ec92b
ibnvakbar <r.ibnuakbar@gmail.com>
2018-02-27 20:56:51 +0700

    initial commit

---
+++ src/main/java/com/example/tutorial3/service/InMemoryStudentService.java
@@ -0,0 +1,38 @@
+package com.example.tutorial3.service;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.tutorial3.model.StudentModel;
+
+public class InMemoryStudentService implements StudentService {
+	private static List<StudentModel> studentList = new ArrayList<StudentModel>();
+
+	@Override
+	public StudentModel selectStudent(String npm) {
+		for(StudentModel i : studentList) {
+			
+			if(i.getNpm().equals(npm)) {
+				return i;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public List<StudentModel> selectAllStudents() {
+		// TODO Auto-generated method stub
+		return studentList;
+	}
+
+	@Override
+	public void addStudent(StudentModel student) {
+		// TODO Auto-generated method stub
+		studentList.add(student);
+	}
+//	nomer 4 localhost:8080/student/view?npm=12345 tidak muncul karena data belum diinsert
+	
+}
